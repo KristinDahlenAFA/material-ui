d484f0139283aa2108aa7e513791613a3762ea53
Kristin Dahlén <kristin.dahlen@afaforsakring.se>
2021-06-23 09:53:32 +0200

    add icons

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,9 @@
 import * as React from "react"
-import { Button, Typography } from "@material-ui/core"
+import { Button, Typography, IconButton } from "@material-ui/core"
 import { ThemeProvider } from "@material-ui/styles"
 import theme from "../theme"
 import { makeStyles } from "@material-ui/core"
+import * as icons from "@material-ui/icons"
 
 import Layout from "../components/layout"
 import Seo from "../components/seo"
@@ -12,6 +13,10 @@ const useStyles = makeStyles({
     color: "red",
   },
 })
+const CreateDynamicIcon = iconName => {
+  const Icon = icons[iconName]
+  return <Icon />
+}
 
 const IndexPage = () => {
   const classes = useStyles()
@@ -20,6 +25,9 @@ const IndexPage = () => {
     <ThemeProvider theme={theme}>
       <Layout>
         <Seo title="Home" />
+        <IconButton className={classes.socialIcon}>
+          {CreateDynamicIcon("HomeIcon")}
+        </IconButton>
         <Typography className={classes.textStyle}>Välkommen hit</Typography>
         <Typography className={classes.textStyle}>Välkommen hit</Typography>
 
