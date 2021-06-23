d70994cced27be96a12f6eabc0e7c4807be916c2
Kristin Dahlén <kristin.dahlen@afaforsakring.se>
2021-06-23 09:32:59 +0200

    add materrial ui

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,5 @@
 import * as React from "react"
+import { Button, Typography } from "@material-ui/core"
 import { ThemeProvider } from "@material-ui/styles"
 import theme from "../theme"
 import { makeStyles } from "@material-ui/core"
@@ -19,6 +20,48 @@ const IndexPage = () => {
     <ThemeProvider theme={theme}>
       <Layout>
         <Seo title="Home" />
+        <Typography className={classes.textStyle}>Välkommen hit</Typography>
+        <Typography className={classes.textStyle}>Välkommen hit</Typography>
+
+        <Typography className={classes.textStyle}>Välkommen hit</Typography>
+
+        <Typography className={classes.textStyle}>Välkommen hit</Typography>
+
+        <Typography className={classes.textStyle}>Välkommen hit</Typography>
+
+        <Typography className={classes.textStyle}>Välkommen hit</Typography>
+
+        <Typography className={classes.textStyle}>Välkommen hit</Typography>
+
+        <Typography className={classes.textStyle}>Välkommen hit</Typography>
+
+        <Button variant="contained" color="primary">
+          Klicka på mig
+        </Button>
+        <Button variant="contained" color="primary">
+          Klicka på mig
+        </Button>
+        <Button variant="contained" color="primary">
+          Klicka på mig
+        </Button>
+        <Button variant="contained" color="primary">
+          Klicka på mig
+        </Button>
+        <Button variant="contained" color="primary">
+          Klicka på mig
+        </Button>
+        <Button variant="contained" color="primary">
+          Klicka på mig
+        </Button>
+        <Button variant="contained" color="primary">
+          Klicka på mig
+        </Button>
+        <Button variant="contained" color="primary">
+          Klicka på mig
+        </Button>
+        <Button variant="contained" color="primary">
+          Klicka på mig
+        </Button>
       </Layout>
     </ThemeProvider>
   )
--- src/pages/page-2.js
+++ src/pages/page-2.js
@@ -1,5 +1,6 @@
 import * as React from "react"
 import { Link } from "gatsby"
+import { Button, Typography } from "@material-ui/core"
 
 import Layout from "../components/layout"
 import Seo from "../components/seo"
@@ -7,8 +8,64 @@ import Seo from "../components/seo"
 const SecondPage = () => (
   <Layout>
     <Seo title="Page two" />
+    <Typography className={classes.textStyle}>Välkommen hit</Typography>
+
+    <Typography className={classes.textStyle}>Välkommen hit</Typography>
+
+    <Typography className={classes.textStyle}>Välkommen hit</Typography>
+
+    <Typography className={classes.textStyle}>Välkommen hit</Typography>
+    <p>Welcome to page 2</p>
+
     <h1>Hi from the second page</h1>
     <p>Welcome to page 2</p>
+    <Button variant="contained" color="primary">
+      Klicka på mig
+    </Button>
+    <Button variant="contained" color="primary">
+      Klicka på mig
+    </Button>
+    <Button variant="contained" color="primary">
+      Klicka på mig
+    </Button>
+    <Button variant="contained" color="primary">
+      Klicka på mig
+    </Button>
+    <Button variant="contained" color="primary">
+      Klicka på mig
+    </Button>
+    <Button variant="contained" color="primary">
+      Klicka på mig
+    </Button>
+    <Button variant="contained" color="primary">
+      Klicka på mig
+    </Button>
+    <Button variant="contained" color="primary">
+      Klicka på mig
+    </Button>
+
+    <Button variant="contained" color="primary">
+      Klicka på mig
+    </Button>
+
+    <Button variant="contained" color="primary">
+      Klicka på mig
+    </Button>
+    <Button variant="contained" color="primary">
+      Klicka på mig
+    </Button>
+    <Button variant="contained" color="primary">
+      Klicka på mig
+    </Button>
+    <Button variant="contained" color="primary">
+      Klicka på mig
+    </Button>
+    <Button variant="contained" color="primary">
+      Klicka på mig
+    </Button>
+    <Button variant="contained" color="primary">
+      Klicka på mig
+    </Button>
     <Link to="/">Go back to the homepage</Link>
   </Layout>
 )
