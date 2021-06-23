6d4169d160e4fbe1f60f4e784b529f0237069bcf
Kristin Dahlén <kristin.dahlen@afaforsakring.se>
2021-06-23 09:28:43 +0200

    remove material ui

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,33 +1,27 @@
 import * as React from "react"
-import { Link } from "gatsby"
-import { StaticImage } from "gatsby-plugin-image"
-import { Button } from "@material-ui/core"
+import { ThemeProvider } from "@material-ui/styles"
+import theme from "../theme"
+import { makeStyles } from "@material-ui/core"
 
 import Layout from "../components/layout"
 import Seo from "../components/seo"
 
-const IndexPage = () => (
-  <Layout>
-    <Seo title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <Button variant="contained" color="primary">
-      Klicka på mig
-    </Button>
-    <StaticImage
-      src="../images/gatsby-astronaut.png"
-      width={300}
-      quality={95}
-      formats={["AUTO", "WEBP", "AVIF"]}
-      alt="A Gatsby astronaut"
-      style={{ marginBottom: `1.45rem` }}
-    />
-    <p>
-      <Link to="/page-2/">Go to page 2</Link> <br />
-      <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
-    </p>
-  </Layout>
-)
+const useStyles = makeStyles({
+  textStyle: {
+    color: "red",
+  },
+})
+
+const IndexPage = () => {
+  const classes = useStyles()
+
+  return (
+    <ThemeProvider theme={theme}>
+      <Layout>
+        <Seo title="Home" />
+      </Layout>
+    </ThemeProvider>
+  )
+}
 
 export default IndexPage
